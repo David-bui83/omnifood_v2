155f00eae6987734f60c92fba8151ffa82fea054
Mona Lisa <david.t.bui83@gmail.com>
2020-05-06 21:41:56 -0700

    registered model with admin

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Message
+
+
+class MessageAdmin(admin.ModelAdmin):
+  list_display = ('id', 'name', 'email', 'find_us', 'news')
+  list_display_links = ('id', 'name', 'email')
+  search_fields = ('name', 'email')
+  list_per_page = 25
 
 # Register your models here.
+admin.site.register(Message, MessageAdmin)
